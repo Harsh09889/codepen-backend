1fdcc6891677644e6bc1d557b179687ff1ada51e
Harsh <harsh09889@gmail.com>
2023-02-18 01:32:24 +0530

    some more debugging

--- index.js
+++ index.js
@@ -11,14 +11,10 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-	handlePreflightRequest: (req, res) => {
-		const headers = {
-			"Access-Control-Allow-Headers": "Content-Type, Authorization",
-			"Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-			"Access-Control-Allow-Credentials": true,
-		};
-		res.writeHead(200, headers);
-		res.end();
+	cors: {
+		origin: "https://cloneofcodepen.netlify.app/",
+		allowedHeaders: ["my-custom-header"],
+		credentials: true,
 	},
 });
 
